50fd047f0189ccc72888dcc0f7d38d5049676e11
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-05-28 16:38:36 +0800

    added Division class

--- index.js
+++ index.js
@@ -35,13 +35,29 @@ class Employee {
     }
 }
 
-class Department extends Employee {
-    constructor(name, division) {
-        super(name);
-        this.division = division
+
+class Division {
+    constructor(name, office, staff) {
+        this.name = name,
+            this.office = office,
+            this.staff = staff
+    }
+
+    getInformation() {
+        console.log({ name: this.name, office: this.office, staff: this.staff })
+    }
+
+    getStaff() {
+        console.log({ staff: this.staff })
     }
 }
 
 const naqiuddin = new Employee("Naqiuddin", 29, "Developer", "Digital Transformation");
+const arthur = new Employee("Arthur", 6, "Cat Developer", "Digital Transformation");
 
-naqiuddin.getEmployee();
+const it = new Division("Information Technology", "Bangsar, MY", [naqiuddin, arthur])
+
+// arthur.getEmployee();
+
+it.getStaff();
+
